ba773756d4e10e8834485da8d2a765ffb7f1c410
natalieretamar <natalie.retamar@gmail.com>
2016-01-03 20:05:27 -0500

    Update advanced_python_csv.py
    uploaded code

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,13 @@
-PLACE YOUR CODE HERE
+#emails.csv
+import csv
+faculty_file = csv.DictReader(open('/Users/natalieabril/ds/metis/prework/dsp/python/faculty.csv'), skipinitialspace = 'True')
+
+outfile = open('emails.csv', 'wb')
+writer = csv.writer(outfile, delimiter = '\n')
+
+emails=[]
+
+for row in faculty_file:
+    emails.append(row['email'])
+
+writer.writerow(emails)
